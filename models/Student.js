88612fc66868ed255f71ae7a04b6bf3cfa37c425
Mridual-code <mridual.14910@stu.upes.ac.in>
2026-06-22 11:58:39 +0530

    Created Student Schema and Model

---
+++ models/Student.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number
+  },
+  course: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  marks: {
+    type: Number,
+    default: 0
+  }
+});
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Student;
